9a0a4ff0cd843928a141c298ae2320173509b758
Pauline Keith Zipagan <paulinekeith.zipagan@infor.com>
2026-04-05 11:53:11 +0800

    add item page

---
+++ pages/item.js
@@ -0,0 +1,54 @@
+import view from "../utils/view.js";
+import Story from "../components/Story.js";
+import baseUrl from "../utils/baseUrl.js";
+
+export default async function Item () {
+    let story = null;
+    let hasComments = false;
+    let hasError = false;
+    let errorMessage = ""
+
+    try {
+        story = await getStory()
+        hasComments = story.comments.length > 0;    
+    } catch (error) {
+        hasError = true;
+        errorMessage = error;
+        console.error(error)
+    }
+
+    if (hasError){
+        view.innerHTML = `
+        <div class="error">
+            Error fetching story!
+            Try looking into this error: ${errorMessage}
+        </div>
+        `
+    }
+
+    view.innerHTML = `
+        <div>
+            ${Story(story)}
+        </div>
+        <hr/>
+        ${hasComments ? story.comments.map(comment => JSON.stringify(comment)).join("") : "No Comments"}
+    `
+}
+
+async function getStory(){
+    console.log(window.location.hash)
+    console.log(window.location.hash.split('?id='))
+    const storyId = window.location.hash.split('?id=')[1]
+    console.log(storyId);
+    
+    //fetch from the api server
+    // const response = await fetch(`https://node-hnapi.herokuapp.com/item/${storyId}`)
+    const response = await fetch(`${baseUrl}/item/${storyId}`)
+    console.log('response for storyId ', response)
+
+    const story = await response.json();
+    console.log('individual story response.json() ', story)
+
+    return story;
+
+}
