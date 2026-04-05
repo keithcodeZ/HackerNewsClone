3a16f8c987ee3f9fe48a33042d1a86399e973d99
Pauline Keith Zipagan <paulinekeith.zipagan@infor.com>
2026-04-05 10:43:47 +0800

    Story component

---
+++ components/Story.js
@@ -0,0 +1,30 @@
+export default function Story(story){
+    // console.log(story);
+    // return `<div>${story}</div>`
+    // return `<div>story</div>`
+
+    return `
+        <div class="story">
+            <div> 
+                <span class="gray">${story.index}</span>
+                <span class="upvote">▲</span>
+                <a target="_blank" href="${story.url}">${story.title}</a>
+                <span>(${story.domain})</span>
+            </div>
+            <div>
+                <div class="gray">
+                ${story.points} points by ${story.user} ${story.time_ago}
+                |
+                <a href="#/item?id=${story.id}">
+                    ${story.comments_count} comments
+                </a>
+                |
+                <span class="favorite">
+                    <img class="heart" src="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/icons/heart-fill.svg">
+                    Add To Favorites
+                </span>
+                </div>
+            </div>
+        </div>
+    `;   
+}
