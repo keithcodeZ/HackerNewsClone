86fcaefbf3a6ef884be77c00c9d3b7ab47fce7a7
Pauline Keith Zipagan <paulinekeith.zipagan@infor.com>
2026-04-05 15:05:41 +0800

    Styling active header

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 import RouterHandler from "./router.js";
 
+window.onhashchange = () => {
+    console.log('page changed!')
+    setActiveLink()
+}
+
+function setActiveLink(){
+    const links = document.querySelectorAll('.header-link');
+    console.log(links)
+
+    links.forEach(link => {
+        const linkPath = link.getAttribute('href');
+        console.log('linkPath: ', linkPath);
+        const currentPath = window.location.hash;
+        console.log('currentPath: ', currentPath);
+        
+        if (currentPath === linkPath) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active')
+        }
+        
+    })
+}
+
 class App {
     constructor() {
         console.log('App entry point!');
